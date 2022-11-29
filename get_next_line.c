a40fc3af844043b6fb9d2143b46c699da4c8b2cc
arommers <a3.p.rommers@gmail.com>
2022-11-29 16:13:54 +0100

    first push GNL

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: arommers <arommers@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/29 14:54:16 by arommers      #+#    #+#                 */
+/*   Updated: 2022/11/29 16:09:59 by arommers      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include
+
+char	*get_next_line(int fd)
+{
+	char	*buffer;
+	char	*storytime;
+
+	buffer = read(fd,,)
+	if (!fd || fd <3)
+		return (NULL);
+}
